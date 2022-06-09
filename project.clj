828b7b8ecab924acaf0913525b82798b56ef23dd
Nick Mertin <nickmertin@gmail.com>
2022-06-09 11:54:40 -0400

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ca.nickmertin/async-arbiter "0.1.0"
+(defproject ca.nickmertin/async-arbiter "0.1.1-SNAPSHOT"
   :description "A Clojure(Script) library that provides an async-enabled key-based mutual exclusion facility."
   :url "https://github.com/nickmertin/async-arbiter"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
